798ddbea5ca4804da07eaa9ec15f3527359e26d8
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-27 08:05:22 +0200

    Implemented removing stay-logged-in item from localStorage after refresh failure.

--- src/hooks/useRefresh.ts
+++ src/hooks/useRefresh.ts
@@ -4,8 +4,13 @@ import { axios } from '../api/axios';
 export const useRefresh = () => {
   const { setAuth } = useAuth();
   return async () => {
-    const { data: axiosData } = await axios.get('auth/refresh', { withCredentials: true });
-    setAuth(axiosData.data);
-    return axiosData.data.accessToken;
+    try {
+      const { data: axiosData } = await axios.get('auth/refresh', { withCredentials: true });
+      setAuth(axiosData.data);
+      return axiosData.data.accessToken;
+    } catch (err) {
+      window.localStorage.removeItem('stay-logged-in');
+      console.log({ err });
+    }
   };
 }
