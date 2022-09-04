aae5c25129030eeccf41123822534f76006d578b
Tomasz Czerwiński <czerwitom@gmail.com>
2022-09-04 10:59:13 +0200

    Removed all users bookmars from admin panel.

--- src/components/Profile/AdminPanel/AdminBookmarks.tsx
+++ src/components/Profile/AdminPanel/AdminBookmarks.tsx
@@ -3,18 +3,13 @@ import { NavLink, Outlet } from 'react-router-dom';
 export const AdminBookmarks = () => {
   return (
       <div className="flex flex-col w-full">
-        <ul className="flex justify-between px-4 flex-wrap list-none gap-4">
-          <li className="text-slate-500">
+        <ul className="flex justify-center px-4 flex-wrap list-none gap-4">
+          <li className="text-slate-500 text-xl">
             <NavLink to="/account/profile" end className="navlink">
               Added posts
             </NavLink>
           </li>
-          <li className="text-slate-500">
-            <NavLink to="/account/profile/users" className="navlink">
-              All users
-            </NavLink>
-          </li>
-          <li className="text-slate-500">
+          <li className="text-slate-500 text-xl">
             <NavLink to="/account/profile/pending-posts" className="navlink">
               Pending posts
             </NavLink>
