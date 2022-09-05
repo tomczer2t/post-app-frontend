33e58716fe4c6113434863c4609e2b3b56e339ef
Tomasz Czerwiński <czerwitom@gmail.com>
2022-09-05 09:56:16 +0200

    Added author username and creation date to specific post.

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -4,6 +4,7 @@ import { axios } from '../../api/axios';
 import { PostsGetSpecificResponse } from 'types';
 import { useParams } from 'react-router-dom';
 import { NotFound } from '../common/NotFound/NotFound';
+import { formatDistance } from 'date-fns';
 
 export const Post = () => {
 
@@ -32,7 +33,13 @@ export const Post = () => {
   return (
     <article className="p-4 max-w-[800px] mx-auto py-12">
       <Loading className="text-8xl mt-16 mx-auto" loading={loading}/>
-      { post && <div className="" dangerouslySetInnerHTML={ { __html: post.content } } /> }
+      { post && <>
+        <div className="" dangerouslySetInnerHTML={ { __html: post.content } } />
+        <p className="text-slate-500 text-slate-400 text-sm">
+            { post.user.username } <span className="text-slate-400">{ formatDistance(new Date(post.createdAt), new Date(), { addSuffix: true }) }</span>
+        </p>
+        <p className=""></p>
+        </> }
       <NotFound error={notFoundError}>
         <p className="text-slate-500">Sorry, post not found.</p>
       </NotFound>
